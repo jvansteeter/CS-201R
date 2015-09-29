ef055df69d656d98902ac77f684011de028d75b2
jvansteeter <spam.magnet1991@gmail.com>
2015-09-28 21:53:01 -0600

    tweeking slider code

--- CS2JQuery/script.js
+++ CS2JQuery/script.js
@@ -5,6 +5,7 @@ $(document).ready(function()
 
   var weatherApp;
   var bounceApp;
+  var sliderApp;
   $.ajax(
   {
       url : "https://api.github.com/repos/jvansteeter/CS-201R/git/blobs/8361c7c4ca28450e3e74c06798e0928b8a1e07f1",
@@ -28,6 +29,17 @@ $(document).ready(function()
     {
       var content = data['data']['content'];
       bounceApp = atob(content);
+    }
+  })
+
+  $.ajax(
+  {
+    url : "https://api.github.com/repos/jvansteeter/CS-201R/git/blobs/b6a770f3121b38d03388db490e82728b32bd11bd",
+    dataType : "jsonp",
+    success : function(data)
+    {
+      var content = data['data']['content'];
+      sliderApp = atob(content);
     }
   })
 
@@ -59,6 +71,13 @@ $(document).ready(function()
       {
         
       });
+   }); // end click function
+
+   $("#sliderbutton").click(function(e)
+   {
+    console.log(String(sliderApp));
+    container.html(String(sliderApp));
+
    }); // end click function
 
 });// end read function
