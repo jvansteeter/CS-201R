ed01c28cc7d36e0b872b87fb7e96004979c104b0
jvansteeter <spam.magnet1991@gmail.com>
2015-09-28 19:17:21 -0600

    dont bother me

--- CS2JQuery/script.js
+++ CS2JQuery/script.js
@@ -1,34 +1,42 @@
 $(document).ready(function()
 {
-   var $body = $("#body");
-   var $button = $("#button");
+  var $body = $("#body");
+  var $button = $(".button");
+
+  var code;
+  var theThing;
+  $.ajax(
+  {
+      url : "https://api.github.com/repos/jvansteeter/CS-201R/git/blobs/9297dfb20aef5ce99eabdfe4ead54739a8f95d3f",
+      dataType : "jsonp",
+      success : function(data)
+      {
+          //console.log(data);
+          var theThing = data['data']['content'];
+          //console.log(theThing);
+          decode = atob(theThing);
+          code = "" + decode;
+          console.log(String(code));
+      }
+  })
    
    $button.click(function(e)
    {
-        $.ajax(
-        {
-            url : "https://api.github.com/users/jvansteeter/gists",
-            dataType : "jsonp",
-            success : function(parsed_json)
-            {
-                console.log(parsed_json);
-               var site = parsed_json['data'][0]['files']['city.html']['raw_url'];
-                console.log(site);
-            }
-        })
-        
-        $.ajax(
-        {
-            url : "https://api.github.com/repos/jvansteeter/CS-201R/git/blobs/3de715ae24c941382f77e70ccce76b9b86324dfb",
-            dataType : "jsonp",
-            success : function(data)
-            {
-                console.log(data);
-                var theThing = data['data']['content'];
-                console.log(theThing);
-                var decode = atob(theThing);
-                console.log(decode);
-            }
-        })
-   });
-});
+      
+      var container = $('#container');
+      console.log(container.html());
+      //$("#container").html("<p>Yep, definitely a test.</p>");
+      
+
+      console.log(String(code));
+      container.html(String(code));
+      console.log(container.html());
+
+      $('.button').each(function(e)
+      {
+        e.preventDefault();
+      })
+        //var display = $('#weather').html(code);
+        //console.log(display);
+   });// end click function
+});// end read function
