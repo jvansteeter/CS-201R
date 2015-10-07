a13e2f07eae2d2fb0d24711ff1669772a01263e4
jvansteeter <spam.magnet1991@gmail.com>
2015-10-07 17:21:43 -0600

    finished angular project

--- CS3Angular/Angular3_Josh/Angular3_Josh/app3.js
+++ CS3Angular/Angular3_Josh/Angular3_Josh/app3.js
@@ -5,10 +5,10 @@ myApp.controller('ProfileController', function($scope, $location, $http) {
   $scope.userProfile = [];
   $scope.repoList = [];
 
-  $scope.onSubmit = function() 
+  $scope.onSubmit = function()
   {
       $scope.fullAddress =  $scope.StreetAddress + ' ' + $scope.City + ' ' + $scope.State + ' ' + $scope.Zip;
-      $scope.userProfile.push({ username:$scope.Username, password:$scope.Password, email:$scope.Email, address:$scope.fullAddress, avatar:$scope.Avatar, 
+      $scope.userProfile.push({ username:$scope.Username, password:$scope.Password, email:$scope.Email, address:$scope.fullAddress, avatar:$scope.Avatar,
       	githubuser:$scope.GithubUser, githubRepo:$scope.GithubRepo});
       console.log($scope.userProfile);
       $scope.toHide = true;
