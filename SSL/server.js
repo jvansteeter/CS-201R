5eb1923d51e4253b37ac01190a5bfe9f960da0b3
jvansteeter <spam.magnet1991@gmail.com>
2015-11-05 22:14:06 -0700

    finished ssl

--- SSL/server.js
+++ SSL/server.js
@@ -1,9 +1,15 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var https = require('https');
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
 var app = express();
+app.use(bodyParser());
+var basicAuth = require('basic-auth-connect');
+var auth = basicAuth(function(user, pass) {
+    return((user ==='cs201r')&&(pass === 'test'));
+  });
 var options = {
     host: '127.0.0.1',
     key: fs.readFileSync('ssl/server.key'),
@@ -14,8 +20,25 @@ var options = {
   app.get('/', function (req, res) {
     res.send("Get Index");
   });
-  app.use('/', express.static('./html', {maxAge: 60*60*1000}));
+  app.use('/', express.static('html', {maxAge: 60*60*1000}));
   app.get('/getcity', function (req, res) {
     console.log("In getcity route");
     res.json([{city:"Price"},{city:"Provo"}]);
+  });
+  app.get('/comment', function (req, res) {
+    console.log("In comment route");
+    resarray = [ { Name: 'Mickey', Comment: 'Hello',
+        _id: '54f53d5ebf89e6100c2180da' },
+      { Name: 'Mark', Comment: 'This is a comment',
+        _id: '54f53e21801a52330c04be8a' }
+      ]
+    res.json(resarray);
+  });
+  app.post('/comment', auth, function (req, res) {
+    console.log("In POST comment route");
+    console.log(req.user);
+    console.log("Remote User");
+    console.log(req.remoteUser);
+    res.status(200);
+    res.end();
   });
